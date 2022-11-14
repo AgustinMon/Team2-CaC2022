8e39bba26a6d073f8182734adcecdcda01b53e13
Gabriel Marchetta <gfmarchetta@gmail.com>
2022-11-13 22:48:44 -0300

    se agrega logo, buscador y navbar

--- proyectofinal/src/Components/Buscador/Buscador.jsx
+++ proyectofinal/src/Components/Buscador/Buscador.jsx
@@ -1,19 +1,28 @@
 import { useState } from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import "./Buscador.css"
 /* Tag Form en uso, no form de HTML */
 
 export const Buscador = () => {
+  const [nombre, setNombre] = useState("");
+  const onInputChange = (e) => {
+    setNombre(e.target.value);
+  };
 
-    const [nombre, setNombre] = useState("");
-    const onInputChange = (e) => {setNombre(e.target.value)};
-
-    return (
-        <form action={`/buscar/${nombre}`}>
-        <fieldset>
-        <input type="text" placeholder="Buscar Pelicula" onChange={onInputChange} value={nombre}/>
+  return (
+    <form action={`/buscar/${nombre}`}>
+      <fieldset class="form-group">
+        <FontAwesomeIcon icon={faSearch} className="Icono" />
+        <input
+          className="InputBuscador"
+          type="text"
+          placeholder="Buscar Pelicula"
+          onChange={onInputChange}
+          value={nombre}
+        />
         <input type="submit" className="btn btn-primary" value="buscar" />
-        </fieldset>
-        </form>
-    )
-}
-
+      </fieldset>
+    </form>
+  );
+};
--- proyectofinal/src/Components/NavBar/Navbar.js
+++ proyectofinal/src/Components/NavBar/Navbar.js
@@ -1,11 +1,24 @@
-import {Link} from "react-router-dom";
-import './navbar.css';
+import "./navbar.css";
+import { Buscador } from "../Buscador/Buscador";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
-export default function Navbar(){
-    return (
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/Login">Login</Link>
-            </nav>
-    )
+export default function NavBar(props) {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home"><img alt="Logo" src={props.src} width={props.size} className="d-inline-block align-top"/></Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/" activeClassname>Home</Nav.Link>
+          <Nav.Link href="../LogIn">Login</Nav.Link>
+        </Nav>
+        <Buscador />
+      </Container>
+    </Navbar>
+  );
 }
+
+NavBar.defaultProps = {
+  size: 90,
+};
