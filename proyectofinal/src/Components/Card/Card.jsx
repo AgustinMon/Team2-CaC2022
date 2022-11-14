b60148cbbbb412cb78bf92291a6c9e91dcc33b07
AgustinMon <67564027+AgustinMon@users.noreply.github.com>
2022-11-14 18:39:55 -0300

    Link en vez de <a en Card y modifico homepage en package.json para probar

--- proyectofinal/src/Components/Card/Card.jsx
+++ proyectofinal/src/Components/Card/Card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Component } from "react";
 import {IMAGE_URL} from "../../Constants/constants";
+import {Link} from "react-router-dom";
 import "./card.css";
 
 export default class Card extends Component {
@@ -21,12 +22,12 @@ export default class Card extends Component {
 
     render() {
         return (
-        <a href={"/View/"+this.id}>
+        <Link to={"/View/"+this.id}>
             <div className="tarjeta">
                 <img className="card-img-top img-thumbnail" src={IMAGE_URL + this.image} alt="Card image cap" />
                 <h5 className="titulo">{this.title}</h5>
             </div>
-        </a>
+        </Link>
         )
     }
 }
