63d37579eb6b70a587d70b1ddc9053025b9cb9b4
Gabriel Marchetta <gfmarchetta@gmail.com>
2022-11-15 02:48:50 -0300

    utilizacion de Link de react routes

--- proyectofinal/src/Components/NavBar/Navbar.js
+++ proyectofinal/src/Components/NavBar/Navbar.js
@@ -5,16 +5,17 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { Link } from "react-router-dom";
 export default function NavBar(props) {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home"><img alt="Logo" src={props.src} width={props.size} className="d-inline-block align-top"/></Navbar.Brand>
+        <Navbar.Brand to="#home"><img alt="Logo" src={props.src} width={props.size} className="d-inline-block align-top"/></Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="/" activeClassname>Home</Nav.Link>
-          <Nav.Link href="../Series">Series</Nav.Link>
-          <Nav.Link href="../Peliculas">Peliculas</Nav.Link>
-          <Nav.Link href="../LogIn">Login</Nav.Link>
+          <Link className="nav-link" to="/" activeClassname>Home</Link>
+          <Link className="nav-link" to="../Series">Series</Link>
+          <Link className="nav-link" to="../Peliculas">Peliculas</Link>
+          <Link className="nav-link" to="../LogIn">Login</Link>
         </Nav>
         <ButtonGroup className="buttonGroup">
         <Buscador />
