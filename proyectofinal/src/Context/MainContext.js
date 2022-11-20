72e25405cbf3965902b4f9720e3cb4aae8a0c475
AgustinMon <67564027+AgustinMon@users.noreply.github.com>
2022-11-20 17:59:33 -0300

    arreglada busqueda, version 0.2.0, correcciones menores

--- proyectofinal/src/Context/MainContext.js
+++ proyectofinal/src/Context/MainContext.js
@@ -7,8 +7,15 @@ const defaultState = {
 }
 
 function getInitialState() {
+    //buscar variable state en el localstorage
+    //la variable state contiene un json con el mismo formato que defaultstate
     const currentState = localStorage.getItem('state');
-    return currentState ? JSON.parse(currentState) : defaultState;
-}
+    //antes de return, se podria grabar un state si no existe
+    //no olvidar JSON.stringify() para guardar en state
+    if (currentState == null) {
+        localStorage.setItem('state', JSON.stringify(defaultState)) 
+    }
 
+    return currentState ?  JSON.parse(currentState) : defaultState;
+}
 export const MainContext = createContext(getInitialState());
