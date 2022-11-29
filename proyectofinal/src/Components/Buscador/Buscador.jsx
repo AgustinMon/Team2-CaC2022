aac4453e5ba9111fc5ef0428a32e68d1c18c550d
AgustinMon <67564027+AgustinMon@users.noreply.github.com>
2022-11-29 19:03:03 -0300

    Mejoras en estilo, correccion perdida contexto

--- proyectofinal/src/Components/Buscador/Buscador.jsx
+++ proyectofinal/src/Components/Buscador/Buscador.jsx
@@ -1,36 +1,43 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 
 import "./Buscador.css"
 /* Tag Form en uso, no form de HTML */
 
 // export const Buscador = () => {
-  export const Buscador = () => {
+export const Buscador = () => {
 
-const [nombre, setNombre] = useState("");
+  const [nombre, setNombre] = useState("");
   const onInputChange = (e) => {
     setNombre(e.target.value);
   };
-  
-  return (
-    <form action={`/buscar/${nombre}`}>
-      <fieldset className="form-group">
-        <input
-          className="InputBuscador"
-          type="text"
-          placeholder="Buscar Pelicula"
-          onChange={onInputChange}
-          value={nombre}
-        />
-        <div className="icono">
-          <Link to={"/buscar/" + nombre} className="lupa"><FontAwesomeIcon icon={faSearch} className="Icono" /></Link>{' '}
-          </div>
 
-      </fieldset>
-    </form>
+  return (
+    <>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <fieldset className="form-group">
+          <input
+            className="InputBuscador"
+            type="text"
+            placeholder="Buscar Pelicula"
+            onChange={onInputChange}
+            value={nombre}
+          />
+          <div className="icono">
+            {
+              nombre.length > 1
+                ?
+                <Link to={"/buscar/" + nombre} className="lupa"><FontAwesomeIcon icon={faSearch} className="Icono" /></Link>
+                :
+                <div className="lupa"><FontAwesomeIcon icon={faSearch} className="Icono" /></div>
+            }
+          </div>
+        </fieldset>
+      </form>
+    </>
   );
 };
 
--- proyectofinal/src/Components/NotFound/NotFound.jsx
+++ proyectofinal/src/Components/NotFound/NotFound.jsx
@@ -1,15 +1,27 @@
+import '../LogIn/login.css';
 import './notfound.css';
 import { CATEGORIAS } from "../../Constants/constants";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import MainContext from "../../Context/MainContext";
+import { LANGUAGES } from "../../Constants/languages";
 
 export const NotFound = () => {
+    const { language, user, changeUser } = useContext(MainContext);
+
     return (
         <>
             <div className="fondo">
                 <div className="contenedor">
-                    <h1>No encontramos lo que buscas :(</h1>
-                    <h2>pero podes probar estas opciones</h2>
-                        {CATEGORIAS.map((elem, i) => <span><Link className="link-light" to={`/Category/${elem.id}`}>{elem.name}</Link></span>)}
+                    <h1>{LANGUAGES[language].NOTFOUND.TITLE}</h1>
+                    <h1>:(</h1>
+                    <h2 className='mt-4'>... {LANGUAGES[language].NOTFOUND.SUBTITLE}:</h2>
+                    <hr />
+                    <ul className='notfound-list mt-4'>
+                        {CATEGORIAS.map((elem, i) => <li key={elem.id}><Link className="link-light" to={`/Category/${elem.id}`}>{elem.name}</Link></li>)}
+                    </ul>
+                    <hr />
+                    <small>{LANGUAGES[language].LOGIN.TITLE}</small>
                 </div>
             </div>
         </>
